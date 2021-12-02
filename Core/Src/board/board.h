b66d015caab3fa5b4b29e535e9b439c85ba7f70d
ASlazyk <95423276+ASlazyk@users.noreply.github.com>
2021-12-02 23:24:42 +0100

    Add files via upload

---
+++ Core/Src/board/board.h
@@ -0,0 +1,22 @@
+/*
+ * board.h
+ *
+ *  Created on: 19 kwi 2017
+ *      Author: Pawe�
+ */
+
+#ifndef SRC_BOARD_BOARD_H_
+#define SRC_BOARD_BOARD_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void board_init();
+ void board_loop();
+
+
+#ifdef __cplusplus
+}
+#endif
+#endif /* SRC_BOARD_BOARD_H_ */
